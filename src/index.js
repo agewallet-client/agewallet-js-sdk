42658e2d19b0a59f8ba2ec4a473ae4663534926a
ashgoodman <ashley@ashleygoodman.net>
2025-12-19 17:00:04 -0700

    Fix IIFE export for browser builds

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import { AgeWallet } from './AgeWallet.js';
 // Export for module bundlers (import { AgeWallet } from '@agewallet/sdk')
 export { AgeWallet };
 
-// Export for browser script tags (window.AgeWallet)
-if (typeof window !== 'undefined') {
-    window.AgeWallet = AgeWallet;
-}
+// Default export for IIFE/browser builds (window.AgeWallet)
+export default AgeWallet;
